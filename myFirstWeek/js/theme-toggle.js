8c1294cf497703b26e8282aa02d2fa70b0820e39
GarethSwarte <garethjswarte@gmail.com>
2026-01-14 23:55:54 +0000

    Updated the contact me form

--- myFirstWeek/js/theme-toggle.js
+++ myFirstWeek/js/theme-toggle.js
@@ -3,10 +3,22 @@
   const toggle = document.querySelector('.toggle-control input[type="checkbox"]');
 
   function applyTheme(name){
-    if(name === 'dark') document.documentElement.setAttribute('data-theme', 'dark');
-    else document.documentElement.removeAttribute('data-theme');
+    const isDark = name === 'dark';
+    if (isDark) {
+      document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+      document.documentElement.removeAttribute('data-theme');
+    }
+    if (logo) {
+      logo.src = isDark ? './logo/logo_500px_dm.svg' : './logo/logo_500px.svg';
+    }
+    if (homepage_pidgeon) {
+homepage_pidgeon.src = isDark ? './img/pidgeon-dark.png' : './img/pidgeon.png';
+}
   }
 
+  
+  const logo = document.querySelector('.logo-img');
   const saved = localStorage.getItem(KEY);
   // default to light if nothing saved
   const start = saved || 'light';
