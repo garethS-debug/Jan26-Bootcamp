7b5bd04292ac078f38f4fddadb4d74db54cf59e8
GarethSwarte <garethjswarte@gmail.com>
2026-01-15 00:29:01 +0000

    attempt to fix about page

--- js/theme-toggle.js
+++ js/theme-toggle.js
@@ -13,8 +13,8 @@
       logo.src = isDark ? './logo/logo_500px_dm.svg' : './logo/logo_500px.svg';
     }
     if (homepage_pidgeon) {
-homepage_pidgeon.src = isDark ? './img/pidgeon-dark.png' : './img/Pidgeon.png';
-}
+      homepage_pidgeon.src = isDark ? './img/pidgeon-dark.png' : './img/pidgeon.png';
+    }
   }
 
   
